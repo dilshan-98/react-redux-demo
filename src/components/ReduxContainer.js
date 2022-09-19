f4215fa7c1d46282554688f5d28fbf09d8b116bf
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-09-19 23:40:35 +0530

    initialized app

---
+++ src/components/ReduxContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReduxContainer = () => {
+    return (
+        <div>
+            <h2>Number of Cakes</h2>
+            <button>But Cake</button>
+        </div>
+    )
+}
+
+export default ReduxContainer;
