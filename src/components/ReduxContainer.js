5dbe525071d3bf8b31c6a84926999ce53ec8f256
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-10-09 15:33:31 +0530

    connected the states with app

--- src/components/ReduxContainer.js
+++ src/components/ReduxContainer.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import buyCake from "../redux/cake/cakeActions";
 
-const ReduxContainer = () => {
+const ReduxContainer = (props) => {
     return (
         <div>
-            <h2>Number of Cakes</h2>
-            <button>But Cake</button>
+            <h2>Number of Cakes - {props.noOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     )
 }
 
-export default ReduxContainer;
+const stateToProps = (state) => {
+    return {
+        noOfCakes: state.noOfCakes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(stateToProps, mapDispatchToProps)(ReduxContainer);
