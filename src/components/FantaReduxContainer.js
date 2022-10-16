f54242dcc12ceae0383875f79b37763a94a8bbab
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-10-16 18:38:51 +0530

    hooks to replace connect and rest of the redux methods.

--- src/components/FantaReduxContainer.js
+++ src/components/FantaReduxContainer.js
@@ -1,26 +1,41 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useSelector, useDispatch } from "react-redux";
 import { buyFanta } from "../redux";
 
-const FantaReduxContainer = (props) => {
+// const FantaReduxContainer = (props) => {
+//     return (
+//         <div>
+//             <h2>Number of Fanta Bottles: {props.noOfFanta}</h2>
+//             <button onClick={props.buyFanta}>Buy</button>
+//         </div>
+//     )
+// }
+
+const FantaReduxContainer = () => {
+    //using Hooks
+    const fantaTotal = useSelector(state => state.fanta.noOfFanta);
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <h2>Number of Fanta Bottles: {props.noOfFanta}</h2>
-            <button onClick={props.buyFanta}>Buy</button>
+            <h2>Number of Fanta Bottles: {fantaTotal}</h2>
+            <button onClick={() => dispatch(buyFanta())}>Buy</button>
         </div>
     )
 }
 
-const stateToProps = (state) => {
-    return {
-        noOfFanta: state.fanta.noOfFanta
-    }
-}
+// const stateToProps = (state) => {
+//     return {
+//         noOfFanta: state.fanta.noOfFanta
+//     }
+// }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        buyFanta: () => dispatch(buyFanta())
-    }
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         buyFanta: () => dispatch(buyFanta())
+//     }
+// }
 
-export default connect(stateToProps, mapDispatchToProps)(FantaReduxContainer);
+// export default connect(stateToProps, mapDispatchToProps)(FantaReduxContainer);
+
+export default FantaReduxContainer;
